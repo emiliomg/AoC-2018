f4a4a93d876bdd8d334ea068e34b46426bec96c4
Emilio Markgraf <emilio.markgraf@fashionid.de>
2018-12-02 21:01:03 +0100

    Simplify parsing

--- src/main/scala/de/emiliomg/adventofcode/y2018/day1/Day1.scala
+++ src/main/scala/de/emiliomg/adventofcode/y2018/day1/Day1.scala
@@ -1,10 +1,11 @@
 package de.emiliomg.adventofcode.y2018.day1
 
 import scala.io.Source
-import scala.util.matching.Regex
-import scala.util.parsing.combinator.RegexParsers
 
 object Day1 extends App {
+
+  type FrequencyShift = Int
+
   val input: List[FrequencyShift] = parseData(getData("2018/1/input.txt"))
   assert(input.length == 1029)
 
@@ -25,14 +26,14 @@ object Day1 extends App {
   println(s"secondStar: $secondStar")
   assert(secondStar == 75108)
 
-  def sumFrequencyShifts(data: List[FrequencyShift]): Int = data.foldRight(0){case (fs, acc) ⇒ acc + fs.shift}
+  def sumFrequencyShifts(data: List[FrequencyShift]): Int = data.foldRight(0){case (fs, acc) ⇒ acc + fs}
 
   def findFirstRevisitedFrequency(data: List[FrequencyShift]): Int = {
 
     def step(partialList: List[FrequencyShift], current: Int, storage: Set[Int]): Int = {
       if (partialList.isEmpty) step(data, current, storage)
       else {
-        val nextFrequency: Int = current + partialList.head.shift
+        val nextFrequency: Int = current + partialList.head
 
         if (storage.contains(nextFrequency)) nextFrequency
         else {
@@ -46,14 +47,5 @@ object Day1 extends App {
   }
 
   def getData(path: String): String = Source.fromResource(path).getLines().toList.mkString(", ")
-  def parseData(data: String): List[FrequencyShift] = TextParser.parse(TextParser.freqShifts, data).get
-}
-
-case class FrequencyShift(shift: Int)
-
-object TextParser extends RegexParsers {
-  override val whiteSpace: Regex = """[\t ]+""".r
-
-  def freqShift: Parser[FrequencyShift] = "[-+][0-9]+".r ^^ (fs ⇒ FrequencyShift(fs.toInt))
-  def freqShifts: Parser[List[FrequencyShift]] = rep(freqShift <~ ",".r.?)
+  def parseData(data: String): List[FrequencyShift] = data.split(", ").map(fs ⇒ fs.toInt).toList
 }
