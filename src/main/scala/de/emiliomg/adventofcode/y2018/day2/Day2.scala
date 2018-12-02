4c25433dc11fec0b87f0da8c1d840397ea7cbdc8
Emilio Markgraf <emilio.markgraf@fashionid.de>
2018-12-02 09:13:30 +0100

    Cleanup

--- src/main/scala/de/emiliomg/adventofcode/y2018/day2/Day2.scala
+++ src/main/scala/de/emiliomg/adventofcode/y2018/day2/Day2.scala
@@ -14,21 +14,20 @@ object Day2 extends App {
   println(s"firstStar: $firstStar")
   assert(firstStar == 5166)
 
-  def findChecksum(testData: List[String]): Int = {
-    // => List(Map(), Map(b -> 3, a -> 2), Map(b -> 2), Map(c -> 3), Map(d -> 2, a -> 2), Map(e -> 2), Map(b -> 3, a -> 3))
-    val count23 = testData.map(_
-      .groupBy(identity)
-      .mapValues(_.length)
-      .filter{ case (_, len) ⇒ len == 2 || len == 3 }
-    )
 
-    count23.flatMap(_
-        .map(_.swap)
-        .keys
-      )
-      .groupBy(identity)
-      .map(_._2.size)
-      .product
+  def findChecksum(testData: List[String]): Int = {
+    testData.map(
+      _.groupBy(identity)
+      .mapValues(_.length)
+      .filter { case (_, len) ⇒ len == 2 || len == 3 }
+    ) // => List(Map(), Map(b -> 3, a -> 2), Map(b -> 2), Map(c -> 3), Map(d -> 2, a -> 2), Map(e -> 2), Map(b -> 3, a -> 3))
+    .flatMap(_
+      .map(_.swap) // remove duplicates of character counts
+      .keys
+    ) // => List(3, 2, 2, 3, 2, 2, 3)
+    .groupBy(identity)
+    .map(_._2.size)
+    .product
   }
 
   def getData(path: String): List[String] = Source.fromResource(path).getLines().toList
