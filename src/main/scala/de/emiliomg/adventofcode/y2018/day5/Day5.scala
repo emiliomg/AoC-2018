68c835becdbfba1e48dac3091bedb7d0c3d72730
Emilio Markgraf <emilio.markgraf@fashionid.de>
2018-12-07 22:10:38 +0100

    Day 5 Star 2

--- src/main/scala/de/emiliomg/adventofcode/y2018/day5/Day5.scala
+++ src/main/scala/de/emiliomg/adventofcode/y2018/day5/Day5.scala
@@ -8,14 +8,23 @@ object Day5 extends App {
 
   val testData: Polymer = getData("2018/5/test.txt")
 
-  assert(reactPolymer(testData).mkString == "dabCBAcaDA")
+  assert(reactPolymerLength(testData) == "dabCBAcaDA".length)
+  assert(bestPolymerLength(testData) == 4)
 
   val data: Polymer = getData("2018/5/input.txt")
-  val firstStar: Int = reactPolymer(data).length
+  val firstStar: Int = reactPolymerLength(data)
+  val secondStar: Int = bestPolymerLength(data)
 
   println(s"First star: $firstStar")
+  println(s"Second star: $secondStar")
 
   assert(firstStar == 9238)
+  assert(secondStar == 4052)
+
+  def bestPolymerLength(data: Polymer): Int = {
+    val polymerUnits = data.map(_.toLower).toSet
+    polymerUnits.map(rmUnit ⇒ data.filterNot(_.toLower == rmUnit)).map(reactPolymerLength).min
+  }
 
   /**
     dabAcCaCBAcCcaDA  The first 'cC' is removed.
@@ -23,11 +32,11 @@ object Day5 extends App {
     dabCBAcCcaDA      Either 'cC' or 'Cc' are removed (the result is the same).
     dabCBAcaDA        No further actions can be taken.
    */
-  def reactPolymer(data: Polymer): Polymer = {
+  def reactPolymerLength(data: Polymer): Int = {
     data.foldRight(List[Char]()){
       case (char, head :: tail) if willReact(char, head) ⇒ tail
       case (char, acc) ⇒ char :: acc
-    }
+    }.length
   }
 
   def willReact(a: Char, b:Char): Boolean = a != b && a.toLower == b.toLower
